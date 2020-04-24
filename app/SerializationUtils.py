e1950dcf2a794153e719db4f06f681ff4e395fad
Asim Shrestha <asim.shrestha@hotmail.com>
2020-04-24 16:06:10 -0700

    refactor: extract method

--- app/SerializationUtils.py
+++ app/SerializationUtils.py
@@ -14,6 +14,14 @@ def getDirectionFromTuple(tuple):
     if tuple[0] == 1 and tuple[1] == 0:
         return 'right'
 
+def getCoordPerimeterCoords(coord):
+    cordPerimeterCoords = []
+    cordPerimeterCoords.append([coord[0], coord[1] - 1]) # Block above the head
+    cordPerimeterCoords.append([coord[0], coord[1] + 1]) # Block below the head
+    cordPerimeterCoords.append([coord[0] - 1, coord[1]]) # Block to the left of head
+    cordPerimeterCoords.append([coord[0] + 1, coord[1]]) # Block to the right of head
+    return cordPerimeterCoords
+
 def getTupleFromDirection(direction):
     if( direction == 'up'):
         return [0,-1]
