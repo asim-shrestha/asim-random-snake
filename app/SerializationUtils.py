42b249654707f4bd5258d4ab5f1f5a3bc7b154dc
Asim Shrestha <asim.shrestha@hotmail.com>
2020-04-24 02:23:34 -0700

    feat: add method to get direction from tuple

--- app/SerializationUtils.py
+++ app/SerializationUtils.py
@@ -4,6 +4,16 @@ def serializeCoordsFromCoordList(coordsList):
             coords.append([coord['x'], coord['y']])
         return coords
 
+def getDirectionFromTuple(tuple):
+    if tuple[0] == 0 and tuple[1] == -1:
+        return 'up'
+    if tuple[0] == 0 and tuple[1] == 1:
+        return 'down'
+    if tuple[0] == -1 and tuple[1] == 0:
+        return 'left'
+    if tuple[0] == 1 and tuple[1] == 0:
+        return 'right'
+
 def getTupleFromDirection(direction):
     if( direction == 'up'):
         return [0,-1]
